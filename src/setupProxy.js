1c6f5df76be130a2e21b4765a0b07a753ba84af8
Aaron A. Brown <kilonow1275@hotmail.com>
2024-08-24 23:53:31 -0600

    add url to api

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     ["/api", "/auth/github", "/github-search", "/graphql"],
     createProxyMiddleware({
-      target: "http://localhost:5000/"
+      target: "https://spotfinder-api.vercel.app"
     })
   );
 };
